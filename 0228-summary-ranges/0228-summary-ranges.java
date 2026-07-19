255fb97f13896fa681ef496c41d8e0db23d2fa5d
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-07-19 14:31:27 +0530

    Time: 3 ms (79.47%), Space: 43.4 MB (8.11%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,32 +1,30 @@
 class Solution {
+
+    private String range(int curr, int next) {
+        if (curr == next - 1) {
+            return "" + curr;
+        }
+        return curr + "->" + (next - 1);
+    }
+
     public List<String> summaryRanges(int[] arr) {
         int n = arr.length;
         ArrayList<String> al = new ArrayList<>();
         if (n == 0)
             return al;
         int curr = arr[0], next = arr[0] + 1;
-        String str;
+
         for (int i = 1; i < n; i++) {
             if (arr[i] == next) {
                 next += 1;
             } else {
-                if (curr == next - 1) {
-                    str = "" + curr;
-                } else {
-                    str = curr + "->" + (next - 1);
-                }
-                al.add(str);
+                al.add(range(curr, next));
                 curr = arr[i];
                 next = arr[i] + 1;
             }
         }
-        if (curr == next - 1) {
-            str = "" + curr;
-        } else {
-            str = curr + "->" + (next - 1);
-        }
-        al.add(str);
 
+        al.add(range(curr, next));
         return al;
     }
 }
