11a9952d568864dfb768d68d88d69c345a6c5a2d
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-07-21 03:19:36 +0530

    Time: 6 ms (35.47%), Space: 45.3 MB (10.57%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        if (n1 < n2)
-            return intersect(nums2, nums1);
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
-        for (int x : nums1) {
-            map1.put(x, map1.getOrDefault(x, 0) + 1);
-        }
-        for (int x : nums2) {
-            map2.put(x, map2.getOrDefault(x, 0) + 1);
-        }
+        HashMap<Integer, Integer> map = new HashMap<>();
         ArrayList<Integer> al = new ArrayList<>();
-        for (var e : map1.entrySet()) {
-            int key = e.getKey();
-            if (map2.containsKey(key)) {
-                for(int i =0; i < Math.min(e.getValue(),map2.get(key)) ; i++){
-                    al.add(key);
-                }
+
+        for (int x : nums1) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+        for (int k : nums2) {
+            if(map.containsKey(k) && map.get(k) > 0){
+                al.add(k);
+                map.replace(k, map.get(k) - 1);
             }
         }
 
