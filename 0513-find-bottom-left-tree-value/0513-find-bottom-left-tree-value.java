1418c010207a9668200973f54130641d6c463d8c
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-07-25 16:00:32 +0530

    Time: 1 ms (81.85%), Space: 46.7 MB (27.99%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,37 +14,32 @@
  * }
  */
 
-class Pair {
-    int data, level;
-
-    public Pair(int d, int l) {
-        data = d;
-        level = l;
-    }
-}
 
 class Solution {
+    int maxLevel = -1;
+    int value = -1;
     private boolean isLeaf(TreeNode root){
         if(root.left == null && root.right == null) return true;
         return false;
     }
-    private void findBLV(TreeNode root,int level,ArrayList<Pair> p){
+    private void findBLV(TreeNode root,int level){
         if(isLeaf(root)){
-            if(level > p.get(0).level){
-                p.get(0).level = level;
-                p.get(0).data = root.val;
+            if(level > maxLevel){
+                maxLevel = level;
+                value = root.val;
             }
             return;
         }
-        if(root.left != null) findBLV(root.left,level +1,p);
-        if(root.right != null) findBLV(root.right,level +1,p);
+        if(root.left != null) findBLV(root.left,level +1);
+        if(root.right != null) findBLV(root.right,level +1);
     }
 
     public int findBottomLeftValue(TreeNode root) {
         ArrayList<Pair> p = new ArrayList<>();
-        p.add(new Pair(root.val,0));
-        findBLV(root,0,p);
+        maxLevel = 0;
+        value = root.val;
+        findBLV(root,0);
         
-        return p.get(0).data;
+        return value;
     }
 }
