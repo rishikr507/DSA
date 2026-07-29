22a0179cb6c3d5cc52bd711388e853d1ee55ee0f
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-07-29 22:38:50 +0530

    Time: 0 ms (100%), Space: 42.7 MB (75.01%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -9,23 +9,36 @@
  * }
  */
 class Solution {
+    int ans = 0;
+
+    private int helper(ListNode head) {
+        if (head == null)
+            return 1;
+
+        int pow = helper(head.next);
+        ans += head.val * pow;
+
+        return pow * 2;
+    }
+
     public int getDecimalValue(ListNode head) {
         if (head == null)
             return 0;
 
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while (temp != null) {
-            st.push(temp.val);
-            temp = temp.next;
-        }
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while (temp != null) {
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
 
-        int ans = 0, pow = 1;
-        ;
-        while (!st.isEmpty()) {
-            ans += st.pop() * pow;
-            pow *= 2;
-        }
+        // int ans = 0, pow = 1;
+        // ;
+        // while (!st.isEmpty()) {
+        //     ans += st.pop() * pow;
+        //     pow *= 2;
+        // }
+        helper(head);
         return ans;
     }
 }
