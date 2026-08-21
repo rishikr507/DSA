73b105d1400071b3f9bd1d9643a2c06b72502ace
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-08-22 00:35:33 +0530

    Time: 0 ms (100%), Space: 45.4 MB (62.64%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -16,12 +16,36 @@ class Solution {
         }
     }
 
+    private void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    private void helper2(int [] arr, int i , List<List<Integer>> res){
+        if(i == arr.length){
+            ArrayList<Integer> al = new ArrayList<>();
+            for(int x: arr){
+                al.add(x);
+            }
+            res.add(al);
+            return;
+        }
+
+        for(int j = i; j < arr.length; j++){
+            swap(arr,i,j);
+            helper2(arr,i+1,res);
+            swap(arr,i,j);
+        }
+    }
+
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        boolean[] flag = new boolean[n];
-        Arrays.fill(flag, false);
-        helper(nums, flag, new ArrayList<>(), res);
+        helper2(nums,0,res);
+        // int n = nums.length;
+        // boolean[] flag = new boolean[n];
+        // Arrays.fill(flag, false);
+        // helper(nums, flag, new ArrayList<>(), res);
         return res;
     }
 }
