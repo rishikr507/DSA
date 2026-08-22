8fe4fd127d4cde3d1db5d7e3e244e321bfc740ba
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-08-23 00:39:00 +0530

    Time: 2 ms (81.54%), Space: 46.6 MB (64.72%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,8 +1,12 @@
 class Solution {
 
-    private void solve(int c, int n, List<String> board, List<List<String>> ans, int[] row, int[] up, int[] down) {
+    private void solve(int c, int n, List<StringBuilder> board, List<List<String>> ans, int[] row, int[] up,
+            int[] down) {
         if (c == n) {
-            ans.add(new ArrayList<>(board));
+            List<String> temp = new ArrayList<>();
+            for (var sb : board)
+                temp.add(new String(sb));
+            ans.add(new ArrayList<>(temp));
             return;
         }
 
@@ -10,9 +14,7 @@ class Solution {
             if (row[r] == 1 || up[n + c - r - 1] == 1 || down[r + c] == 1)
                 continue;
             else {
-                StringBuilder sb = new StringBuilder(board.get(r));
-                sb.setCharAt(c, 'Q');
-                board.set(r, sb.toString());
+                board.get(r).setCharAt(c, 'Q');
                 row[r] = 1;
                 up[n + c - r - 1] = 1;
                 down[r + c] = 1;
@@ -21,24 +23,23 @@ class Solution {
                 row[r] = 0;
                 up[n + c - r - 1] = 0;
                 down[r + c] = 0;
-                sb.setCharAt(c, '.');
-                board.set(r, sb.toString());
+                board.get(r).setCharAt(c, '.');
             }
         }
     }
 
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        List<String> board = new ArrayList<>();
+        List<StringBuilder> board = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             char[] row = new char[n];
             Arrays.fill(row, '.');
-            board.add(new String(row));
+            board.add(new StringBuilder(new String(row)));
         }
         int row[] = new int[n];
         int[] upward = new int[2 * n - 1];
         int[] downward = new int[2 * n - 1];
-        
+
         solve(0, n, board, ans, row, upward, downward);
         return ans;
     }
