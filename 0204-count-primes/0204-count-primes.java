255e8a85acd0ce8b79fea430dd1d5ebb0a74cac0
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-08-29 20:56:17 +0530

    Time: 743 ms (8.92%), Space: 79.8 MB (8.58%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,21 +1,35 @@
 class Solution {
     public int countPrimes(int n) {
-        // if (n == 0 || n == 1)
-        //     return 0;
-        int cnt = 0;
-        boolean[] prime = new boolean[n + 1];
-        Arrays.fill(prime, true);
-        for (int i = 2; i * i < n; i++) {
-            if (prime[i] == true) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
+        boolean[] arr = new boolean[n+1];
+        Arrays.fill(arr , true);
+        for(int i=2 ; i*i<n ; i++){
+            if(arr[i] == true){
+                for(int j=i*i ; j<n ; j+=i){
+                    arr[j] = false;
                 }
             }
         }
-        for (int i = 2; i < n; i++) {
-            if (prime[i] == true)
-                cnt++;
+        int ans = 0;
+        for(int i=2 ; i<n ; i++){
+            if(arr[i] == true) ans++;
         }
-        return cnt;
+        return ans;
+        // if (n == 0 || n == 1)
+        //     return 0;
+        // int cnt = 0;
+        // boolean[] prime = new boolean[n + 1];
+        // Arrays.fill(prime, true);
+        // for (int i = 2; i * i < n; i++) {
+        //     if (prime[i] == true) {
+        //         for (int j = i * i; j < n; j += i) {
+        //             prime[j] = false;
+        //         }
+        //     }
+        // }
+        // for (int i = 2; i < n; i++) {
+        //     if (prime[i] == true)
+        //         cnt++;
+        // }
+        // return cnt;
     }
 }
