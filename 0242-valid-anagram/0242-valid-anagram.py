0e5ee8446299d71ee6ed797909c7af7468a4413a
Rishi Kumar <145141358+rishikr507@users.noreply.github.com>
2026-09-20 23:54:58 +0530

    Time: 9 ms (81.44%), Space: 19.4 MB (76.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,20 +4,14 @@ class Solution:
             return False
         freq1 = {}
         for c in s:
-            if c in freq1:
-                freq1[c] += 1
-            else:
-                freq1[c] = 1
+            freq1[c] = freq1.get(c, 0) + 1
 
         freq2 = {}
         for c in t:
-            if c in freq2:
-                freq2[c] += 1
-            else:
-                freq2[c] = 1
+            freq2[c] = freq2.get(c, 0) + 1
 
-        for k in freq1.keys():
-            if k not in freq2 or freq2[k] != freq1[k]:
-                return False
+        # for k in freq1.keys():
+        #     if k not in freq2 or freq2[k] != freq1[k]:
+        #         return False
 
-        return True
+        return freq1 == freq2
